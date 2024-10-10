0b69955ef4bf3bbf42a71822b95593fd203ac857
SujayCh07 <chavasujay91@gmail.com>
2024-10-10 10:30:00 -0400

    Adding Simulating Craps code

---
+++ src/FileInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.*;
+public class FileInput{
+    public static void main(String[] args){
+        String inputLine;
+        Scanner fileInput;
+        File inFile = new File("TestData.txt");
+        System.out.println("Opening and reading file");
+        try{
+            fileInput = new Scanner(inFile);
+            while(fileInput.hasNext()){
+                inputLine = fileInput.nextLine();
+                System.out.println(inputLine);
+            } // end while
+            fileInput.close();
+        } // end try
+        catch (FileNotFoundException e){
+            System.out.println(e);
+            System.exit(1);
+        } // end catch
+        System.out.println("end of program");
+    }
+}
--- src/FileScannerProgram.java
+++ src/FileScannerProgram.java
@@ -1,37 +1,30 @@
-// Text File Input
-//
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.*;
-
-public class FileIO {
-
+public class FileScannerProgram{
     public static void main(String[] args){
-
         String inputLine;
-
         Scanner fileInput;
-        File inFile = new File("TestData.txt");
-
+        ArrayList<String> maze = new ArrayList<String>();
+        File inFile = new File("simpleMaze.txt");
         System.out.println("Opening and reading file");
-
         try{
             fileInput = new Scanner(inFile);
-
             while(fileInput.hasNext()){
-                inputLine = fileInput.next();
+                inputLine = fileInput.nextLine();
+                maze.add(Arrays.toString(inputLine.split("")));
                 System.out.println(inputLine);
             } // end while
-
             fileInput.close();
-
         } // end try
-
         catch (FileNotFoundException e){
             System.out.println(e);
-            System.exit(1);		// IO error; exit program
+            System.exit(1);
         } // end catch
-
+        for (int i = 0; i < maze.size(); i++){
+            System.out.println(maze.get(i));
+        }
         System.out.println("end of program");
     }
 }
