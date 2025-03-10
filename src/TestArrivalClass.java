327a9be88dfd54e1d6675cd2c50cd7b122a61cb0
SujayCh07 <chavasujay91@gmail.com>
2025-03-10 07:56:31 -0400

    Fixed Arrival Test Class

--- src/TestArrivalClass.java
+++ src/TestArrivalClass.java
@@ -1,34 +1,35 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.io.*;
+import java.util.Scanner;
+
 public class TestArrivalClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String inputLine;
         Scanner fileInput;
-        ArrayList<String> customers = new ArrayList<String>();
+        Arrival customers = new Arrival(0, 0);
         File inFile = new File("Customers.txt");
+
         System.out.println("Opening and reading file");
-        try{
+
+        try {
             fileInput = new Scanner(inFile);
-            while(fileInput.hasNextLine()){
+            while (fileInput.hasNextLine()) {
                 inputLine = fileInput.nextLine();
-                customers.add(Arrays.toString(inputLine.split("")));
+                String[] customerData = inputLine.split(" ");
+                if (customerData.length == 3) {
+                    int arrivalTime = Integer.parseInt(customerData[0]);
+                    int serviceTime = Integer.parseInt(customerData[1]);
+                    String name = customerData[2];
+                    customers.addCustomer(arrivalTime, serviceTime, name);
+                }
                 System.out.println(inputLine);
-            } // end while
+            }
             fileInput.close();
-        } // end try
-        catch (FileNotFoundException e){
+        }
+        catch (FileNotFoundException e) {
             System.out.println(e);
             System.exit(1);
-        } // end catch
-        /*
-        for (int i = 0; i < customers.size(); i++){
-            System.out.println(customers.get(i));
         }
-         */
-        System.out.println("end of program");
 
-
+        System.out.println("End of program");
     }
 }
